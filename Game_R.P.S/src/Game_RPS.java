1483cc0e697826077f8f54451ff11aeabf3fa2f4
Julius <Julius>
2020-05-10 13:25:14 +0300

    Game Rock paper scissors / step 1

--- Game_R.P.S/src/Game_RPS.java
+++ Game_R.P.S/src/Game_RPS.java
@@ -3,10 +3,37 @@
  * @author Julius
  * Project: Game Rock Rock paper scissors
  */
+
+    import java.util.Scanner;
+
 public class Game_RPS {
 
     public static void main(String[] args) {
-        System.out.println("Heloo Git");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("0 - Pabaiga");
+        System.out.println("1 - Akmuo");
+        System.out.println("2 - Popierius");
+        System.out.println("3 - Zirkles");
+        System.out.println("--------GAME_RPS---------------");
+        
+        
+        int kompiuteris = (int)Math.floor(Math.random() * 3 + 1);
+        
+//        System.out.println(kompiuteris);
+
+        
+                int pasirinkimas = sc.nextInt();
+                System.out.println(kompiuteris);
+                System.out.println("---------");
+                if(kompiuteris > pasirinkimas) {
+                    System.out.println("Pralaimejai");
+                } else if (kompiuteris == pasirinkimas) {
+                    System.out.println("Lygiosios");
+                } else {
+                    System.out.println("laimejai");
+                }
+        
+                
     }
     
 }
