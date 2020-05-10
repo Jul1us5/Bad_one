6096a3e3d2a283af38187af60bb9e1999e676c7b
Julius <Julius>
2020-05-10 16:54:00 +0300

    Game Rock paper scissors / step 2

--- Game_R.P.S/src/Game_RPS.java
+++ Game_R.P.S/src/Game_RPS.java
@@ -10,28 +10,78 @@ public class Game_RPS {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("0 - Pabaiga");
-        System.out.println("1 - Akmuo");
-        System.out.println("2 - Popierius");
-        System.out.println("3 - Zirkles");
-        System.out.println("--------GAME_RPS---------------");
+        System.out.print("[ 0 - Pabaiga ]");
+        System.out.print("[ 1 - Akmuo ]");
+        System.out.print("[ 2 - Popierius ]");
+        System.out.println("[ 3 - Zirkles ]");
+        System.out.println("-----------------GAME_RPS---------------");
         
         
-        int kompiuteris = (int)Math.floor(Math.random() * 3 + 1);
+//        int kompiuteris = (int)Math.floor(Math.random() * 3 + 1);
         
 //        System.out.println(kompiuteris);
 
         
-                int pasirinkimas = sc.nextInt();
+//                int pasirinkimas = sc.nextInt();
+//                System.out.println(kompiuteris);
+//                System.out.println("---------");
+//                if(kompiuteris > pasirinkimas) {
+//                    System.out.println("Pralaimejai");
+//                } else if (kompiuteris == pasirinkimas) {
+//                    System.out.println("Lygiosios");
+//                } else {
+//                    System.out.println("laimejai");
+//                }
+
+//            boolean pabaiga = true;
+//            int kompiuteris = (int)Math.floor(Math.random() * 3 + 1);
+            for(int i=0; i<=i; i++) {
+                   
+                   int kompiuteris = (int)Math.floor(Math.random() * 3 + 1);
+                   int pasirinkimas = sc.nextInt();
                 System.out.println(kompiuteris);
-                System.out.println("---------");
-                if(kompiuteris > pasirinkimas) {
-                    System.out.println("Pralaimejai");
-                } else if (kompiuteris == pasirinkimas) {
-                    System.out.println("Lygiosios");
-                } else {
-                    System.out.println("laimejai");
+//                System.out.println("---------");
+                
+           
+                if(pasirinkimas == 1 && kompiuteris == 1) {
+                    System.out.println("- -[ Lygiosios! ]- -");
                 }
+                if(pasirinkimas == 1 && kompiuteris == 2) {
+                    System.out.println("- -[ Pralaimejai.. ]- -");
+                }
+                if(pasirinkimas == 1 && kompiuteris == 3) {
+                    System.out.println("- -[ Laimejai! ]- -");
+                }
+                if(pasirinkimas == 2 && kompiuteris == 1) {
+                    System.out.println("- -[ Laimejai! ]- -");
+                }
+                if(pasirinkimas == 2 && kompiuteris == 2) {
+                    System.out.println("- -[ Lygiosios ]- -");
+                }
+                if(pasirinkimas == 2 && kompiuteris == 3) {
+                    System.out.println("- -[ Pralaimejai.. ]- -");
+                }
+                if(pasirinkimas == 3 && kompiuteris == 1) {
+                    System.out.println("- -[ Pralaimejai.. ]- -");
+                }
+                if(pasirinkimas == 3 && kompiuteris == 2) {
+                    System.out.println("- -[ Laimejai! ]- -");
+                }
+                if(pasirinkimas == 3 && kompiuteris == 3) {
+                    System.out.println("- -[ Lygiosios ]- -");
+                }                 
+
+
+                
+                if(pasirinkimas == 0) {
+                    System.out.println("Pabaiga");
+                    break;
+                } 
+                if(pasirinkimas >= 4 || pasirinkimas < 0) {
+                    System.out.println("!!! [ Toks variantas negalimas ] !!!");
+                    continue;
+                } 
+            }
         
                 
     }
