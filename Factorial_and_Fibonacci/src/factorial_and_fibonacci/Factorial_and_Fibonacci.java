d0e99adda1b17a86da34ffb1ea66d03b0c6a4091
Julius <Julius>
2020-05-11 15:48:49 +0300

    Factorial and Fibonacci numbers

---
+++ Factorial_and_Fibonacci/src/factorial_and_fibonacci/Factorial_and_Fibonacci.java
@@ -0,0 +1,13 @@
+package factorial_and_fibonacci;
+
+/**
+ *
+ * @author Julius
+ */
+public class Factorial_and_Fibonacci {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
